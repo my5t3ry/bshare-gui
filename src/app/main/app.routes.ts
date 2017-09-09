5ebc734ba6d2542fa7c8b707dee6266d292008bc
my5t3ry <sash@mischkonsum.org>
2017-09-09 15:00:35 +0200

    init

--- src/app/main/app.routes.ts
+++ src/app/main/app.routes.ts
@@ -3,9 +3,10 @@ import {NoContentComponent} from '../no-content';
 
 import {PreferencesComponent} from "../preferences/preferences.component";
 import {NavComponent} from "./nav.component";
+import {ProjectsComponent} from "../projects/projects.component";
 
 export const ROUTES: Routes = [
-  {path: '', component: NavComponent},
+  {path: '', component: ProjectsComponent},
   {path: 'preferences', component: PreferencesComponent},
   {path: '**', component: NoContentComponent},
 ];
